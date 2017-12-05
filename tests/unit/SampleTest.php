5b892edfbb774a2c5e12bb0c540c468ecbe896ec
Hari Krishna <harikrishna.a@westagilelabs.com>
2017-12-05 17:24:05 +0530

    Modified Circle CI config file

--- tests/unit/SampleTest.php
+++ tests/unit/SampleTest.php
@@ -4,7 +4,7 @@ use PHPUnit\Framework\TestCase;
 class SampleTest extends TestCase
 {
   public function testTrueAssertsToTrue() {
-    $this->assertTrue(true);
+    $this->assertTrue(false);
   }
 }
 
